24c9a2cdd67f1c1624326aced73e0d7207a319ef
danmae <daniel@lostingravity.de>
2021-11-12 17:38:05 +0100

    Pipeline Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>intelligent Level Learning Feedback</h1>
         <p>Die Umfrage dauert ca. 5 Minuten und beinhaltet 14 Fragen.</p>
-        <button>Start</button>
+        <button>Umfrage starten</button>
       </header>
     </div>
   );
