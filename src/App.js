d4d6b17539992e2eead0759157f961a17c4b96e1
danmae <daniel@lostingravity.de>
2021-11-12 17:36:57 +0100

    Fix start screen

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>intelligent Level Learning Feedback</h1>
+        <p>Die Umfrage dauert ca. 5 Minuten und beinhaltet 14 Fragen.</p>
+        <button>Start</button>
       </header>
     </div>
   );
