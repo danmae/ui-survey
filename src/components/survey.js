15d3d36d96b02bdf9f4e8c05b4e71152b65ece90
danmae <daniel@lostingravity.de>
2021-12-30 21:22:06 +0100

    hotfix

--- src/components/survey.js
+++ src/components/survey.js
@@ -3,6 +3,7 @@ import './survey.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 
+
 class Survey extends Component {
     /*
     1. Datum an dem mit iLL gearbeitet wurde (indirekt)
@@ -55,21 +56,6 @@ class Survey extends Component {
     return (
         <div className='wrapper'>
           <h1 className='heading'>Umfrage - intelligent Level Learning</h1>
-          <Router>
-            <Switch>
-              <Route path="/question_one" exact component={question_one}/>
-              <Route path="/question_two" component={question_two}/>
-              <Route path="/question_three" component={question_three}/>
-              <Route path="/question_four" component={question_four}/>
-              <Route path="/question_five" component={question_five}/>
-              <Route path="/question_six" component={question_six}/>
-              <Route path="/question_seven" component={question_seven}/>
-              <Route path="/question_eight" component={question_eight}/>
-              <Route path="/question_nine" component={question_nine}/>
-              <Route path="/question_ten" component={question_ten}/>
-              <Route path="/question_eleven" component={question_eleven}/>
-            </Switch>
-          </Router>
           <Link to='/question_one'>Befragung Starten</Link>
 
         </div>
